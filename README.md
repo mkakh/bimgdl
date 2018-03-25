88fe2115da9d91c59342d710e7bead169aa17dfc
akh <akhs3214@gmail.com>
2018-03-25 18:19:49 +0900

    Fix critical bugs

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ python bimgdl.py 検索語
 # 参考URL
 
 https://gist.github.com/genekogan/ebd77196e4bf0705db51f86431099e57
+
+# Google画像版
+https://github.com/mkakh/gimgdl
--- bimgdl.py
+++ bimgdl.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 import json
 import os
 import sys
@@ -8,12 +9,12 @@ import imghdr
 
 argv = sys.argv
 argc = len(argv)
-if argc != 3:
-    print("Usage: python", argv[0], "searchterm", "pic_num")
+if argc != 2:
+    print("Usage: python", argv[0], "searchterm")
     exit(1)
 
 searchterm = sys.argv[1]
-pic_num = int(sys.argv[2])
+pic_num = 1000 #目安
 
 url = "https://www.bing.com/images/search?q="+searchterm+""
 # NEED TO DOWNLOAD CHROMEDRIVER, insert path to chromedriver inside parentheses in following line
@@ -26,30 +27,30 @@ header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KH
 counter = 0
 succounter = 0
 
-browser.execute_script("window.scrollTo(0, document.body.scrollHeight)");
+# ループ回数とスリープ時間は環境依存
+for _ in range(pic_num // 30 + 1):
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)");
+    sleep(1)
 
 if not os.path.exists(searchterm):
     os.mkdir(searchterm)
 
-while succounter <= pic_num:
-    for x in browser.find_elements_by_xpath('//a[@class="iusc"]'):
-        counter = counter + 1
-        print("Total Count:", counter)
-        print("Succsessful Count:", succounter)
-        print("URL:", json.loads(x.get_attribute('m'))['murl'])
-        img = json.loads(x.get_attribute('m'))['murl']
-        try:
-            req = requests.get(img, params={'User-Agent': header})
-            raw_img = req.content
-            imgtype = imghdr.what(None, h=raw_img)
-            with open(os.path.join(searchterm , searchterm + "_" + str(counter) + "." + imgtype), "wb") as File :
-                File.write(raw_img)
-                succounter = succounter + 1
-        except:
-            print("can't get img")
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight)");
+for x in browser.find_elements_by_xpath('//a[@class="iusc"]'):
+    counter = counter + 1
+    print("Total Count:", counter)
+    print("Succsessful Count:", succounter)
+    print("URL:", json.loads(x.get_attribute('m'))['murl'])
+    img = json.loads(x.get_attribute('m'))['murl']
+    try:
+        req = requests.get(img, params={'User-Agent': header})
+        raw_img = req.content
+        imgtype = imghdr.what(None, h=raw_img)
+        with open(os.path.join(searchterm , searchterm + "_" + str(counter) + "." + imgtype), "wb") as f :
+            f.write(raw_img)
+            succounter = succounter + 1
+    except:
+        print("can't get img")
 
 print(succounter, "pictures succesfully downloaded")
 browser.execute_script('window.alert("finish!")')
 browser.close()
-
